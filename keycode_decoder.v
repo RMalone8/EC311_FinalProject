3b50e6978905e7ffee9d03665a25f1b7b4c8dd0f
RMalone8 <131809387+RMalone8@users.noreply.github.com>
2023-12-13 16:58:45 -0500

    Update keycode_decoder.v

--- keycode_decoder.v
+++ keycode_decoder.v
@@ -27,33 +27,35 @@ output [4:0] a_or_b_out
     );
 
 reg [4:0]abreg;
-parameter a =  5'b00000;
+
+parameter a = 5'b00000;
 parameter b = 5'b00001;
 parameter c = 5'b00010;
 parameter d = 5'b00011;
 parameter e = 5'b00100;
 parameter f = 5'b00101;
-parameter g = 5'b00111;
-parameter h = 5'b01000;
-parameter i = 5'b01001;
-parameter j = 5'b01010;
-parameter k = 5'b01011;
-parameter l = 5'b01111;
-parameter m = 5'b10000;
-parameter n = 5'b10001;
-parameter o = 5'b10010;
-parameter p = 5'b10011;
-parameter q = 5'b10100;
-parameter r = 5'b10101;
-parameter s = 5'b10110;
-parameter t = 5'b10111;
-parameter u = 5'b11000;
-parameter v = 5'b11001;
-parameter w = 5'b11010;
-parameter x = 5'b11011;
-parameter y = 5'b11100;
-parameter z = 5'b11101;
+parameter g = 5'b00110;
+parameter h = 5'b00111;
+parameter i = 5'b01000;
+parameter j = 5'b01001;
+parameter k = 5'b01010;
+parameter l = 5'b01011;
+parameter m = 5'b01100;
+parameter n = 5'b01101;
+parameter o = 5'b01110;
+parameter p = 5'b01111;
+parameter q = 5'b10000;
+parameter r = 5'b10001;
+parameter s = 5'b10010;
+parameter t = 5'b10011;
+parameter u = 5'b10100;
+parameter v = 5'b10101;
+parameter w = 5'b10110;
+parameter x = 5'b10111;
+parameter y = 5'b11000;
+parameter z = 5'b11001;
 parameter ENTR = 5'b11111;
+
 always @(dig1,dig2) begin
     case(dig2) //leftmost character of keystroke
     1: begin case(dig1) //rightmost character of keystroke
@@ -61,7 +63,7 @@ always @(dig1,dig2) begin
         13: abreg <= w;
         12: abreg<= a;
         11:abreg<=s; 
-        26: abreg<=z;
+        10: abreg<=z;
         endcase
         end
     2: begin case(dig1)
@@ -88,7 +90,7 @@ always @(dig1,dig2) begin
         end
     4: begin case(dig1)
         3: abreg <= i;
-        4: abreg <= 0;
+        4: abreg <= o;
         13:abreg<= p;
         2:abreg<=k;
         11: abreg<=l;
