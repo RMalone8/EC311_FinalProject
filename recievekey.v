922297a75c4cf81a3c89a4fc716574bdf8ed85cc
cbands21 <153328771+cbands21@users.noreply.github.com>
2023-12-13 15:48:40 -0500

    Update recievekey.v

--- recievekey.v
+++ recievekey.v
@@ -3,28 +3,6 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 03/03/2015 09:08:56 PM
-// Design Name: 
-// Module Name: debouncer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
 // Create Date: 10/24/2023 09:20:46 AM
 // Design Name: 
 // Module Name: revievekey_test_mod
@@ -49,7 +27,8 @@ output [3:0] num1, //first digit reading right to left
 output [3:0] num2, // second digit reading right to left
 output [3:0] num3,// first digit of "a" (previous number) reading right to left
 output [3:0] num4, // second digit reading right to left
-output kr
+//output [15:0] krcode
+output reg kr
     );
 
 reg[8:0] key_up_down;
@@ -58,7 +37,12 @@ assign num1 [3:0] = keycode[3:0];
 assign num2 [3:0] = keycode[7:4];
 assign num3 [3:0] = keycode[27:24];
 assign num4 [3:0] = keycode[31:28];
+//assign krcode [15:0] = keycode[23:8];
 
-assign kr = (keycode[26:8]==16'b0010101101000101);
-
+always@(keycode)begin
+    if(keycode[15:12]==16'b1111)
+        kr <=1 ;
+      else
+      kr<=0;
+end
 endmodule
