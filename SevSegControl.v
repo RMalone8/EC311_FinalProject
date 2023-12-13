74090a3a7df545d35da55b74fc977073bbfa2701
cbands21 <153328771+cbands21@users.noreply.github.com>
2023-12-13 17:53:59 -0500

    Working 7 segment control

--- SevSegControl.v
+++ SevSegControl.v
@@ -5,24 +5,25 @@ module SevSegControl(
     input [11:0] wpm_integer,  // 12-bit BCD (3 digits) for integer part of WPM
     input [7:0] wpm_decimal,   // 8-bit BCD (2 digits) for decimal part of WPM
     output reg [6:0] SEG,      // 7-segment display segments
-    output reg [4:0] AN        // 5 Anodes for 7-segment display
+    output reg [4:0] AN,        // 5 Anodes for 7-segment display
+    output reg [2:0] NAN = 4'hF,
+    output reg DP
 );
 
     // Internal registers for individual digits
     wire [3:0] digit1, digit2, digit3, digit4, digit5;
-
+    
     // Extracting individual digits from BCD inputs
     //assign digit1 = wpm_integer[3:0];     // Least significant digit of integer part
     //assign digit2 = wpm_integer[7:4];     // Middle digit of integer part
     //assign digit3 = wpm_integer[11:8];    // Most significant digit of integer part
     //assign digit4 = wpm_decimal[3:0];     // Least significant digit of decimal part
     //assign digit5 = wpm_decimal[7:4];     // Most significant digit of decimal part
-    assign digit1 = 10'd1;     // Least significant digit of integer part
-    assign digit2 = 10'd3;     // Middle digit of integer part
-    assign digit3 = 10'd1;    // Most significant digit of integer part
-    
-    assign digit4 = 10'd1;     // Least significant digit of decimal part
-    assign digit5 = 10'd5;     // Most significant digit of decimal part
+    assign digit1 = wpm_decimal[3:0];     // Least significant digit of integer part
+    assign digit2 = wpm_decimal[7:4];     // Middle digit of integer part
+    assign digit3 = wpm_integer[3:0];    // Most significant digit of integer part
+    assign digit4 = wpm_integer[7:4];     // Least significant digit of decimal part
+    assign digit5 = wpm_integer[11:8];     // Most significant digit of decimal part
 
     
     // Function for mapping digits to 7-segment patterns
@@ -63,26 +64,32 @@ module SevSegControl(
             3'b000 : begin
                 SEG <= digit_to_seg(digit1);
                 AN <= 5'b11110;
+                DP <= 1;
             end
             3'b001 : begin
                 SEG <= digit_to_seg(digit2);
                 AN <= 5'b11101;
+                DP <= 1;
             end
             3'b010 : begin
                 SEG <= digit_to_seg(digit3);
                 AN <= 5'b11011;
+                DP <= 0;
             end
             3'b011 : begin
                 SEG <= digit_to_seg(digit4);
                 AN <= 5'b10111;
+                DP <= 1;
             end
             3'b100 : begin
                 SEG <= digit_to_seg(digit5);
                 AN <= 5'b01111;
+                DP <= 1;
             end
             default : begin
                 SEG <= 7'b1111111; // Turn off segment
                 AN <= 5'b11111;    // Turn off all anodes
+                DP <= 1;
             end
         endcase
     end
